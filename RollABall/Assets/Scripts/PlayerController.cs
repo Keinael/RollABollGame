4cc667ab5cf7fabeb1421c930644704d28eae3c1
Keinael <usenko.ua@gmail.com>
2017-11-14 19:44:08 +0200

    Creating player controller script, adding plane and wals

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate ()
+    {
+        float moveAxisX = Input.GetAxis("Horizontal");
+        float moveAxisZ = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveAxisX, 0.0f, moveAxisZ);
+
+        rb.AddForce(movement * speed);
+    }    
+}
