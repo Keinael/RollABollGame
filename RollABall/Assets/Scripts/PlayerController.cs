75ac3e50287c98a5c4609da65a7c30c6236970ff
Keinael <usenko.ua@gmail.com>
2017-11-16 16:23:38 +0200

    Game is done. Added pickUps object, and UI elements.

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
+    private int count;
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
-    
-    void FixedUpdate ()
+
+    void FixedUpdate()
     {
         float moveAxisX = Input.GetAxis("Horizontal");
         float moveAxisZ = Input.GetAxis("Vertical");
-        
+
         Vector3 movement = new Vector3(moveAxisX, 0.0f, moveAxisZ);
 
         rb.AddForce(movement * speed);
-    }    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            gameObject.transform.localScale += new Vector3(0.2f, 0.2f, 0.2f);
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
 }
